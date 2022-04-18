14814c34c28108cba27557c55fd7bcccabfd8a77
yuri.exe <mahdiabid158@gmail.com>
2022-04-18 04:22:12 +0100

    update

---
+++ js/chart.js
@@ -0,0 +1,60 @@
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+];
+
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'grey',
+        borderColor: 'white',
+        data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+};
+
+const config = {
+    type: 'line',
+    data: data,
+    options: {}
+};
+
+const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
+
+
+var ctx = document.getElementById("doughnutChart");
+var doughnutChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'],
+        datasets: [{
+            label: '# of Tomatoes',
+            data: [12, 19, 3, 5],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        //cutoutPercentage: 40,
+        responsive: false,
+
+    }
+});
